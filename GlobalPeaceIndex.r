255eb327511333720b3ec99e4b7ebdc96680acc0
Ryo-N7 <ryonakagawara@gmail.com>
2017-09-18 12:32:21 +0100

    - close to finish edits

--- GlobalPeaceIndex.r
+++ GlobalPeaceIndex.r
@@ -242,3 +242,24 @@ font_import() # only needs to be run once
 loadfonts(device = "win")    # needed to add (device = win)!!!
 
 
+
+GPI_rank %>% 
+  filter(rank <= 10) %>% 
+  mutate(jpn = ifelse(country == "Japan", T, F)) %>% 
+  ggplot(aes(year, rank, group = country)) +
+  geom_line(aes(color = jpn, alpha = jpn), size = 2) +
+  geom_point(aes(color = jpn, alpha = jpn), size = 2.3) +
+  geom_text(data = GPI_rank %>% filter(year == "2008", rank <= 10), 
+            aes(label = country, x = "2008"), color = "black", size = 4, nudge_x = -1.5) +
+  geom_text_repel(data = GPI_rank %>% filter(year == "2017", rank <= 10), 
+                  aes(label = country, x = "2017"), color = "black", size = 4, nudge_x = 1.5) +
+  scale_y_reverse(breaks = pretty_breaks(10)) +
+  scale_x_discrete(expand = c(0.15, 0.1)) +
+  scale_color_manual(values = c("#104E8B", "#EE2C2C")) +     # set all non-Japan to blue, Japan to red
+  labs(x = "Year", y = "Rank") +
+  ggtitle("Global Peace Index (Top 10)", subtitle = "(2008-2017)") +
+  theme_peace
+
+
+
+
